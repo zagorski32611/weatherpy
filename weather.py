687c1f70a2d6d06ccff63f34f465b1ff6341c8e9
Joe Zagorski <j.j.zagorski@gmail.com>
2018-02-04 08:51:09 -0500

    added icons folder, working on notification bar for ubuntu

--- weather.py
+++ weather.py
@@ -1,9 +1,31 @@
 import requests
 import json
+import appindicator
 import pynotify
+import gtk
 
+# Create new app indicator with name and icon:
+app = appindicator.Indicator("weatherpy", '/home/joe/weatherpy/icons/sun.png', appindicator.CATEGORY_APPLICATION_STATUS)
+app.set_status(appindicator.STATUS_ACTIVE)
+
+# Create app menu:
+menu = gtk.Menu()
+gw = gtk.MenuItem("Get Weather")
+qi = gtk.MenuItem("Quit")
+
+# append gw and qi to Menu
+menu.append(gw)
+menu.append(qi)
+
+# set menu to application:
+app.set_menu(menu)
+gw.show()
+qi.show()
+
+# Get info from Dark Sky, only daily forcasts by exclude minutely and hourly forecasts:
 r = requests.get("https://api.darksky.net/forecast/2b41959f8298b697485d46bea2477504/41.443234,-81.774603?exclude=minutely,hourly")
 
+# Get response and convert to json
 def get_weather():
     if r.status_code != 200:
         print "Shit's broke!!"
@@ -19,6 +41,7 @@ def parse_weather():
     global summary
     current_temp = weather["currently"]["temperature"]
     summary = weather["currently"]["summary"]
+    icon = weather["currently"]["icon"]
 
 print parse_weather()
 
