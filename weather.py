05e3e7397e1a816bb66ea43336d65d6f5461f757
Joe Zagorski <j.j.zagorski@gmail.com>
2018-02-04 07:34:50 -0500

    parsed json to show current temp, pop up works

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import pynotify
-'''
+
 r = requests.get("https://api.darksky.net/forecast/2b41959f8298b697485d46bea2477504/41.443234,-81.774603?exclude=minutely,hourly")
 
 def get_weather():
@@ -14,12 +14,18 @@ def get_weather():
 
 def parse_weather():
     weather = get_weather()
-    return weather #["hourly"]["data"][0]['precipType']
-
+    #return weather["hourly"]["data"][0]['precipType']
+    global current_temp
+    current_temp = weather["currently"]["temperature"]
+    
 print parse_weather()
-'''
+
+
 def sendmessage(title, message):
     pynotify.init("Test")
     notice = pynotify.Notification(title, message)
     notice.show()
+    title = "Current Weather"
+    message = parse_weather()
     return
+sendmessage("Current Weather", str(current_temp))
