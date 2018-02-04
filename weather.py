6a3b10c6274fde37f779a8d5f7b26f426158e12c
Joe Zagorski <j.j.zagorski@gmail.com>
2018-02-04 07:38:53 -0500

    added string to output

--- weather.py
+++ weather.py
@@ -16,10 +16,13 @@ def parse_weather():
     weather = get_weather()
     #return weather["hourly"]["data"][0]['precipType']
     global current_temp
+    global summary
     current_temp = weather["currently"]["temperature"]
-    
+    summary = weather["currently"]["summary"]
+
 print parse_weather()
 
+w_message = "It is currently %s degress" % current_temp + " with " + summary
 
 def sendmessage(title, message):
     pynotify.init("Test")
@@ -28,4 +31,4 @@ def sendmessage(title, message):
     title = "Current Weather"
     message = parse_weather()
     return
-sendmessage("Current Weather", str(current_temp))
+sendmessage("Current Weather", w_message)
