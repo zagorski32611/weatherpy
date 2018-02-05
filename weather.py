47b28ff741c27f83840bc2b737f4131083409ce5
Joe Zagorski <j.j.zagorski@gmail.com>
2018-02-04 19:59:22 -0500

    added functionality for weather alerts

--- weather.py
+++ weather.py
@@ -22,11 +22,13 @@ def parse_weather():
     global current_icon
     global future_summary
     global future_icon
+    global alert
     current_temp = weather["currently"]["temperature"]
     summary = weather["currently"]["summary"]
     current_icon = weather["currently"]["icon"]
-    future_summary = [1]
-    future_icon = [1]
+    future_summary = weather["daily"]["summary"]
+    future_icon = weather["daily"]["icon"]
+    alert = weather["alerts"][0]["description"]
 
 #{u'currently': {u'ozone': 366.78, u'windGust': 5.39, u'temperature': 36.33, u'dewPoint': 34.33, u'nearestStormDistance': 23, u'humidity': 0.92, u'nearestStormBearing': 321, u'summary': u'Mostly Cloudy', u'apparentTemperature': 36.33, u'pressure': 1007.64, u'windSpeed': 2.97, u'precipProbability': 0, u'visibility': 4.69, u'cloudCover': 0.78, u'time': 1517774124, u'windBearing': 286, u'precipIntensity': 0, u'uvIndex': 1, u'icon': u'partly-cloudy-day'}, u'daily': {u'icon': u'snow', u'data': #[{u'apparentTemperatureMinTime': 1517803200, u'precipType': u'snow', u'temperatureLow': 15.42, u'precipIntensityMaxTime': 1517760000, u'temperatureMin': 20.7, u'temperatureHigh': 36.66, u'summary':
 
@@ -44,26 +46,45 @@ def get_icon(input):
     elif input == "rain" or input == "sleet":
         return "/home/joe/weatherpy/icons/drizzle.png"
     elif input == "snow":
-        return "/home/joe/weatherpy/icons/snow.png"
+        return "/home/joe/weatherpy/weatherpy/icons/snow.png"
     elif input == "wind":
-        return "/home/joe/weatherpy/icons/wind.png"
+        return "/home/joe/weatherpy/weatherpy/icons/wind.png"
     elif input == "fog":
-        return "/home/joe/weatherpy/icons/fog.png"
+        return "/home/joe/weatherpy/weatherpy/icons/fog.png"
     elif input == "cloudy":
-        return "/home/joe/weatherpy/icons/cloudy.png"
+        return "/home/joe/weatherpy/weatherpy/icons/cloudy.png"
     elif input == "partly-cloudy-day":
-        return "/home/joe/weatherpy/icons/partlycloudy_day.png"
+        return "/home/joe/weatherpy/weatherpy/icons/partlycloudy_day.png"
     elif input == "partly-cloudy-night":
-        return "/home/joe/weatherpy/icons/partlycloudy_night.png"
+        return "/home/joe/weatherpy/weatherpy/icons/partlycloudy_night.png"
     else:
-        return "/home/joe/weatherpy/icons/sun.png"
+        return "/home/joe/weatherpy/weatherpy/icons/sun.png"
 
 # Message for notification bubble
 w_message = "It is currently %s degress" % current_temp + " with " + summary
 current_image = get_icon(current_icon)
-future_message = "Tomorrow will be %s  " % future_summary
+future_message = future_summary
 future_icon = get_icon(future_icon)
+alert_message = alert
+
+# Check if there are alerts:
+def check_alerts(alert):
+    try:
+        alert
+    except NameError:
+        return alert
+
+
 # Create the notificaiton bubble:
+
+def sendalert(title, message, image):
+    notify2.init("Test")
+    notice = notify2.Notification(title, message, image)
+    notice.show()
+    title = "ALERT!"
+    return
+sendalert("ALERT!", alert, future_icon)
+
 
 def sendweather(title, message, image):
     notify2.init("Test")
@@ -80,4 +101,15 @@ def sendtomweather(title, message, image):
     notice.show()
     title = "Tomorrow's weather"
     return
-sendtomweather("Tomorrow's Weather", future_message, future_icon)
+sendtomweather("This week: ", future_message, future_icon)
+
+'''
+DOESn't work :(
+def sendalert(title, message, image):
+    notify2.init("Test")
+    notice = notify2.Notification(title, message, image).set_urgency(URGENCY_CRITICAL)
+    notice.show()
+    title = "ALERT!"
+    return
+sendalert("Current Alert!", alert, future_icon)
+'''
